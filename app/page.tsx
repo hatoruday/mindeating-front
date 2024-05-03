e0d70a33a03aac3e39bde36d91b20306df7cd4db
hatoruday <gkgkeogns@naver.com>
2024-05-03 11:42:37 +0900

    home editing2

--- app/page.tsx
+++ app/page.tsx
@@ -15,22 +15,29 @@ export default function HomePage() {
   // const [isFadeOut, setIsFadeOut] = useState<boolean>(false);
   //set slash to !slash whey clicked
   return (
-    <div className="flex flex-col justify-center">
-      <div className="flex justify-center">
-        <Image src="/cheerIcon.svg" alt="cheerIcon" width={300} height={300} />
+    <div className="bg-white px-10">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="flex pt-10 justify-end">
+          <Image src="/CheerIcon.svg" alt="Your Company" width={200} height={200} className="ml-20 h-[121px] w-auto" priority={true} />
+        </div>
+        <h3 className="text-[#9F9FAC] text-[16px] te">마음먹기에 오신 걸 환영해요</h3>
+        <h1 className="text-black font-semibold text-[26px]">마음먹기를 시작해볼까요?</h1>
       </div>
-      <div>
+      <div className="mt-6">
+        <div>
+          {/* {error && <p className="text-red-500 text-center text-sm mt-5">{error}</p>} */}
+          <div className="flex items-center space-x-4 md:space-x-2 justify-between mx-auto w-full my-8"></div>
+        </div>
+      </div>
+      <div className="flex w-full justify-center py-5">
         <span>결제 페이지로 가기</span>
       </div>
-      <div className="flex flex-col w-full justify-center content-center">
-        <span>LOGIN</span>
-        <span>INFO</span>
-        <span>CONTENTS</span>
-        <span>MIND</span>
-        <span>ACTIVITY</span>
-        <span>SLEEP</span>
-        <span>EMOTION</span>
-      </div>
+      <small className="text-center block text-xs text-black3 font-medium">
+        주문번호를 잊었나요?{" "}
+        <a href="" className="underline font-semibold text-black3 hover:text-black2">
+          결제 알림톡을 확인해 보세요!
+        </a>
+      </small>
     </div>
   );
 }
